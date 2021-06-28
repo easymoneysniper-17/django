330dfec4ba524adc5c7b51006378d9b0ae1735cb
easymoneysniper-17 <jzy20@mails.tsinghua.edu.cn>
2021-06-28 20:24:43 +0800

    '测试‘

---
+++ HelloWorld/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views,testdb,search,search2
+ 
+urlpatterns = [
+    url(r'^hello/$', views.runoob),
+    url(r'^testdb/$', testdb.testdb),
+    url(r'^search-form/$', search.search_form),
+    url(r'^search/$', search.search),
+    url(r'^search-post/$', search2.search_post),
+]
